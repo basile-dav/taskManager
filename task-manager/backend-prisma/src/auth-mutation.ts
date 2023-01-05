2446446ee1fe27af60b353c25fa63cd02e849a88
LAPTOP-HMD27CKP\basil <basiledavid.2000@gmail.com>
2023-01-05 12:24:10 +0100

    auth push

--- task-manager/backend-prisma/src/auth-mutation.ts
+++ task-manager/backend-prisma/src/auth-mutation.ts
@@ -5,17 +5,15 @@ import { FindUniqueUsersArgs } from '@generated/resolvers/crud/Users/args/FindUn
 import {Users} from "@generated/models/Users";
 import { Ctx } from "type-graphql";
 import { getPrismaFromContext } from "@generated/helpers";
-import { Module } from "module";
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { APP_SECRET, getUserId } = require('../utils')
+import bcrypt from 'bcrypt';
+
 
 @TypeGraphQL.Resolver()
 export class AuthenticationResolver {
     @TypeGraphQL.Mutation({
         nullable:false
     })
-    async signup(@TypeGraphQL.Ctx() ctx: any, @TypeGraphQL.Info() info: GraphQLResolveInfo, @TypeGraphQL.Args() args: any) {
+    async signup(@TypeGraphQL.Ctx() ctx: any, @TypeGraphQL.Info() info: GraphQLResolveInfo, @TypeGraphQL.Args() args: CreateOneUsersArgs) {
         const password = await bcrypt.hash(args.password,10)
         const user = await getPrismaFromContext(ctx).users.create({...args,password})
         const token = jwt.sign({userId:user.id},APP_SECRET)
